9b075bce9f0d05fe9aa07983dc367c549511a288
Matthew Groholski <mgrohols@asu.edu>
2023-05-09 18:01:24 -0700

    Initial Commit

---
+++ PeachSconeMarket/PeachSconeMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeachSconeMarketApp.swift
+//  PeachSconeMarket
+//
+//  Created by Matt Groholski on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PeachSconeMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
